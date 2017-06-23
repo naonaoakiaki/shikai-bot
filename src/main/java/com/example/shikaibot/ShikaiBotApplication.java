4bdb2feee5d1be0029a62bf529feb96627f5853f
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2017-06-24 06:27:23 +0900

    first commit

---
+++ src/main/java/com/example/shikaibot/ShikaiBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.shikaibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShikaiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShikaiBotApplication.class, args);
+	}
+}
